ec482d4f6457d2c5d27bf9f98d8662bab36b3c92
Marwa Nasser <marwa.nasser8133@gmail.com>
2026-05-02 22:30:24 +0300

    Fix url

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getToken = () => localStorage.getItem("auth_token");
 const API_URL =
-  import.meta.env.VITE_API_URL || "https://alserage.alfanar-rec.com/api/public";
+  import.meta.env.VITE_API_URL || "https://alserage.alfanar-rec.com";
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
